faa5b0f653b759a360f744b772695db0b98a9bff
eandrade21 <erickandrade21@gmail.com>
2015-02-02 15:26:31 -0800

    Initial Commit

---
+++ SnakeSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SnakeSwift
+//
+//  Created by PartyMan on 2/2/15.
+//  Copyright (c) 2015 PartyLand. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
